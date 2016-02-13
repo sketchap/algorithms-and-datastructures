e5716a028d1a4f4b50c45b037e30356079087721
Silvio Tschapke <silvio.tschapke@gmail.com>
2016-02-13 21:25:09 +0100

    Initial commit

--- test/client/algorithmen.spec.js
+++ test/client/algorithmen.spec.js
@@ -1,7 +1,18 @@
 import algorithmen from '../../src/algorithmen.js';
 
 describe('algorithmen', () => {
+
+  function verifySorted(array){
+    for(var i = 0; i < array.length - 1; i++){
+      assert(array[i] <= array[i + 1], 'array is not sorted');
+    }
+  }
+
   it('should be runing without any problems', () => {
     expect(algorithmen).to.not.throw();
+  });
+
+  it('should sort by integer', function() {
+    verifySorted([1, 3, 4, 6]);
   });
 });
